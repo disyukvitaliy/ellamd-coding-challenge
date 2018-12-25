559527cfca7846cd453ac92bc22a5535f729d31d
Disyuk Vitaliy <v.disyuk@idfly.ru>
2018-12-25 11:16:44 +0700

    Web::FormulationsController#index uses includes in query.

--- backend/app/controllers/web/formulations_controller.rb
+++ backend/app/controllers/web/formulations_controller.rb
@@ -1,6 +1,6 @@
 class Web::FormulationsController < Web::ApplicationController
   def index
-    render json: Formulation.all,
+    render json: Formulation.all.includes(ingredient_relations: :ingredient),
       each_serializer: FormulationSerializer,
       include: [ ingredient_relations: :ingredient ]
   end
